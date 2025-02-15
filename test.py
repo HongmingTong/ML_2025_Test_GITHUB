247b0d62dfd370ca5dac53383e3f100b25ce48aa
Hongming <htong1@ualberta.ca>
2025-02-14 18:35:22 -0600

    Class is created

--- test.py
+++ test.py
@@ -64,3 +64,25 @@ for a in range(0,3,1):
 
 
 
+
+def main():
+    class Person:
+
+        def __init__(self, name, age):
+            self.name = name
+
+            self.age = age
+
+
+    class Student(Person):
+        def __init__(self, name, age, id):
+            super().__init__(name, age)
+            self.id = id
+
+    obj1 = Student("John", 23, 1234)  # object instance from the Student class
+
+    dict_att = obj1.__dict__
+    print(dict_att)
+    return dict_att
+
+main()
