9c29def32550b8eb34e01c94d2fbccfcc241fa50
Hongming <htong1@ualberta.ca>
2025-06-28 15:05:41 -0600

    capmain

---
+++ main.py
@@ -0,0 +1,33 @@
+from Analyzer import df, data_export, showcorrplot, printinfo, encodedf, showpairplot, showhistplot, showboxplot, \
+    shuffledf, sampledf
+#printinfo()
+#encodedf()
+#showcorrplot()
+#showpairplot()
+#showhistplot()
+#showboxplot()
+#--------------
+#shuffledf()
+#sampledf()
+#data_export()
+
+from Classifier import logisitic, DTreeC, KNNC, RFC, SVClassifier, ANNClassifier
+#logisitic()
+#KNNC()
+#DTreeC()
+#RFC()
+#SVClassifier()
+#ANNClassifier()
+
+from Regressor import linear, KNNR, DTreeR, RFR, SVRegression, ANNRegression
+#linear()
+#KNNR()
+#DTreeR()
+#RFR()
+#SVRegression()
+#ANNRegression()
+
+from Clustering import kmeancluster, Meanshift, Hierarchyagglomerative
+#kmeancluster()
+#Meanshift()
+#Hierarchyagglomerative()
